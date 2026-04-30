2d71727b433df6aa4385e6ffde4defc4438f75f6
Helton Reis <hrkings@rhaizzing.com>
2026-04-30 20:02:24 -0300

    test: bring the newer test corpus from typst

--- test/import_typst_tests.py
+++ test/import_typst_tests.py
@@ -5,10 +5,10 @@ import re
 import subprocess
 
 root_dir = Path(__file__).parents[1]
-tests_dir = root_dir.parent / "typst" / "tests" / "suite"
+tests_dir = root_dir / "typst-source-temp" / "tests" / "suite"
 tests_files = list(tests_dir.glob("**/*.typ"))
 with open(root_dir / "test" / "corpus" / "typst.scm", "w") as file:
     for test in tests_files:
         for name, typ in re.findall(r"--- ([A-Za-z0-9-]+) (?:render )?(?:html )?(?:large )?---(.+?(?=---|$))", test.read_text(), re.DOTALL):
-            file.write(f"{'=' * 20}\ntypst/{test.stem}/{name}\n{'=' * 20}\n{typ}\n{'-' * 20}\n\n")
+            file.write(f"{'=' * 20}\ntypst/{test.stem}/{name}\n{'=' * 20}\n{typ.lstrip(chr(10))}\n{'-' * 20}\n\n")
 subprocess.run(["tree-sitter", "test", "-i^typst/", "-u"], cwd=root_dir)
